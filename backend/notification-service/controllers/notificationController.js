b8599a2c41a538b5234c37602b440710c0a9f1df
ZidanAli909 <ahmadzidanali909@gmail.com>
2025-06-16 22:30:09 +0700

    renamed:    backend/notification-service/debugging/consumer.js -> backend/notification-service/consumer.js 	modified:   backend/notification-service/controllers/notificationController.js 	modified:   backend/notification-service/prisma/schema.prisma 	modified:   backend/notification-service/server.js

--- backend/notification-service/controllers/notificationController.js
+++ backend/notification-service/controllers/notificationController.js
@@ -1,5 +1,5 @@
 const { PrismaClient } = require("@prisma/client");
-const { publishNotification } = require('../configs/rabbidmq');
+// const { publishNotification } = require('../configs/rabbidmq');
 
 const prisma = new PrismaClient();
 
@@ -13,7 +13,7 @@ exports.getNotifications = async (req, res) => {
 exports.deleteNotification = async (req, res) => {
     const id = parseInt(req.params.id);
     try {
-        await prisma.notification.delete({ where: { notifID: id } });
+        await prisma.notification.delete({ where: { notifId: id } });
         res.status(204).end();
     } catch (err) {
         res.status(404).json({ error: 'Notification not found!' });
@@ -21,19 +21,19 @@ exports.deleteNotification = async (req, res) => {
 }
 
 exports.postNotification = async (req, res) => {
-    const { itemID, message } = req.body;
+    const { itemId, message } = req.body;
     
-    if (!itemID || !message) {
-        return res.status(400).json({ error: "itemID and message are required" });
+    if (!itemId || !message) {
+        return res.status(400).json({ error: "itemId dan message dibutuhkan!" });
     }
     
     // Save to DB
     const newNotification = await prisma.notification.create({
-        data: { itemID, message },
+        data: { itemId, message },
     });
     
     // Publish to RabbidMQ
-    await publishNotification({ itemID, message });
+    // await publishNotification({ itemId, message });
 
     res.status(201).json(newNotification);
 }
--- backend/notification-service/server.js
+++ backend/notification-service/server.js
@@ -11,5 +11,5 @@ app.use("/api/notification", require("./routes/notificationRoutes"));
 /*
     Port Exposure
 */
-const PORT = process.env.PORT || 5005;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
