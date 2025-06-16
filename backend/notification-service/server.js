db8b554d6f065505c0f964301729e57a8ed6b0ae
ardavv <aryadavi276@gmail.com>
2025-06-16 23:34:07 +0700

    all services config and railway deployment

--- backend/notification-service/server.js
+++ backend/notification-service/server.js
@@ -12,4 +12,4 @@ app.use("/api/notification", require("./routes/notificationRoutes"));
     Port Exposure
 */
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
